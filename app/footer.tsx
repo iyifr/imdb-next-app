f6b8d52704c4d08b321ea42e6e4a343863c719fa
Iyimide Adekile <105992164+iyifr@users.noreply.github.com>
2023-02-25 17:21:47 +0100

    footer completed

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,22 +1,39 @@
 import React from "react" ;
-import { FaTwitter , FaInstagram , FaFacebookF, FaYoutube, FaExternalLinkAlt} from 'react-icons/fa'
+import { FaTwitter , FaInstagram , FaFacebookF, FaYoutube, FaExternalLinkAlt, FaRegCopyright} from 'react-icons/fa'
 import {SiTiktok} from 'react-icons/si'
 
 const Footer = () => {
     return <>
-    <div className = "bg-black w-full m-0 mt-5 p-5">
+    <div className = "bg-black w-full m-0 mt-5 p-5 fixed left-0 bottom-0">
+                                {/* Social media icons section*/}
         <div className="mx-auto container max-w-xl p-2">
             <div className = "mx-auto text-center flex flex-row justify-center space-x-10">
-                <FaTwitter className="text-white cursor-pointer text-3xl"/>
                 <SiTiktok className="text-white cursor-pointer text-2xl"/>
-                <FaFacebookF className="text-white cursor-pointer text-2xl"/>
-                <FaYoutube className="text-white cursor-pointer text-2xl"/>
                 <FaInstagram className="text-white cursor-pointer text-2xl"/>
+                <FaTwitter className="text-white cursor-pointer text-2xl hover:text-blue-400"/>
+                <FaYoutube className="text-white cursor-pointer text-2xl hover:text-red-500"/>
+                <FaFacebookF className="text-white cursor-pointer text-2xl hover:text-blue-500"/>
             </div>
-            <div className="mt-4 w-[70%] mx-auto flex flex-wrap text-white text-bold text-xl">
-                <p>Help me please</p>
-                <p>Help me please</p>
-                <p>Help me please</p>
+
+                            {/* Links section of the footer */}
+            <div className="mt-4 w-[90%] mx-auto flex justify-center flex-wrap space-y-2 text-white font-semibold text-lg space-x-5" >
+                <p className= "mt-2 cursor-pointer">Get the IMDb app <FaExternalLinkAlt className="inline ml-1 mb-2 text-sm"/></p>
+                <p className = "cursor-pointer hover:text-amber-200">Help <FaExternalLinkAlt className="inline mb-1.5 text-sm ml-1"/></p>
+                <p className = "cursor-pointer hover:text-amber-200">Site Index <FaExternalLinkAlt className="inline mb-1.5 text-sm ml-1"/></p>
+                <p className = "cursor-pointer hover:text-amber-200">IMDb pro <FaExternalLinkAlt className="inline mb-1.5 text-sm ml-1" /></p> 
+                <p className = "cursor-pointer hover:text-amber-200">Box Office Mojo <FaExternalLinkAlt className="inline mb-1.5 text-sm ml-1"/></p>
+                <p className = "cursor-pointer hover:text-amber-200">IMDb developer <FaExternalLinkAlt className="inline mb-1.5 text-sm ml-1"/></p> 
+                <p className = "cursor-pointer hover:text-amber-200">Press Room <FaExternalLinkAlt className="inline mb-1.5 text-sm ml-1" /></p>
+                <p className = "cursor-pointer hover:text-amber-200">Advertising <FaExternalLinkAlt className="inline mb-1.5 text-sm ml-1"/></p> 
+                <p className = "cursor-pointer hover:text-amber-200">Jobs <FaExternalLinkAlt className="inline mb-1.5 text-sm ml-1" /></p> 
+                <p className = "cursor-pointer hover:text-amber-200">Conditions of use <FaExternalLinkAlt className="inline mb-1.5 text-sm ml-1" /></p>
+                <p className = "cursor-pointer hover:text-amber-200">Privacy Policy <FaExternalLinkAlt className="inline mb-1.5 text-sm ml-1"/></p> 
+                <p className = "cursor-pointer hover:text-amber-200">Your Ads Privacy Choices</p>
+            </div>
+
+            <div className = "flex justify-center mt-5">
+                <p className="text-sm font-thin text-white font-bold">
+                <FaRegCopyright className="inline text-sm" /> 1999 - {new Date().getFullYear()} by IMdb.com, Inc</p>
             </div>
         </div>
     </div>
